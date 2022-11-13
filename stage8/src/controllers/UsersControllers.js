d061005501256b5f1e94d6885a7efbb5df35710c
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-13 15:27:11 -0300

    UsersControllers create function

--- stage8/src/controllers/UsersControllers.js
+++ stage8/src/controllers/UsersControllers.js
@@ -1,6 +1,40 @@
+const { hash, compare } = require("bcrypt")
+const sqliteConn = require("../database/sqlite");
+const AppError = require("../utils/AppError")
+
 class UsersControllers {
-  async getUsers(req, res) {
+  async checkStatus(req, res) {
     res.json({message: 'Server OK'})
+  }
+
+  async create(req, res) {
+    const { name, email, password, avatar } = req.body;
+
+    console.log(req.body)
+
+    const db = await sqliteConn();
+
+    const checkUserExists = await db.get(`
+      SELECT * FROM users
+      WHERE email = ?
+      `,
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError(`Email ${name} already in use`, 406)
+    };
+
+    const hashedPassword = await hash(password, 8)
+    
+    await db.run(`
+      INSERT INTO users (name, email, password, avatar)
+      VALUES (?,?,?,?)`,
+      [name, email, hashedPassword, avatar ?? null]
+    );
+
+    return res.status(201).json();
+
   }
 }
 
--- stage8/src/routes/users.routes.js
+++ stage8/src/routes/users.routes.js
@@ -5,7 +5,8 @@ const usersRoutes = Router();
 const UsersControllers = require("../controllers/UsersControllers")
 const usersControllers = new UsersControllers();
 
-usersRoutes.get("/", usersControllers.getUsers);
+usersRoutes.get("/-", usersControllers.checkStatus);
+usersRoutes.get("/", usersControllers.create);
 
 
 module.exports = usersRoutes;
