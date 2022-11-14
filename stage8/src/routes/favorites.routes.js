aeaf3c20b8ea43e62a4ccf2f9b236abc45f1ef9e
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-14 14:52:12 -0300

    FavoriteMoviesControllers methods created

--- stage8/src/routes/favorites.routes.js
+++ stage8/src/routes/favorites.routes.js
@@ -5,6 +5,9 @@ const favoritesRoutes = Router();
 const FavoriteMoviesControllers = require("../controllers/FavoriteMoviesControllers");
 const favoriteMoviesControllers = new FavoriteMoviesControllers();
 
-favoritesRoutes.post('/', favoriteMoviesControllers.create);
+favoritesRoutes.get('/:user_id', favoriteMoviesControllers.read)
+favoritesRoutes.post('/', favoriteMoviesControllers.create)
+favoritesRoutes.put('/', favoriteMoviesControllers.update)
+favoritesRoutes.delete('/:fav_id', favoriteMoviesControllers.delete);
 
 module.exports = favoritesRoutes;
