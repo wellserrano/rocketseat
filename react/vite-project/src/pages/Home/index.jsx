c5618786b8da6011fd89874cb3d21aa96b3e533f
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-17 18:05:23 -0300

    Brand, Menu and NewNotes created

--- react/vite-project/src/pages/Home/index.jsx
+++ react/vite-project/src/pages/Home/index.jsx
@@ -1,9 +1,47 @@
-import { Container, Brand, Header, Menu, Search, Content, NewNote } from './styles'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+//Components
+import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText'
+
+//Icons
+import { FiPlus } from 'react-icons/fi'
 
 export function Home() {
   return (
     <Container>
-      <Brand/>
+
+      <Brand>
+        Rocketseat
+      </Brand>
+
+      <Header/>
+
+      <Menu>
+
+        <li>
+          <ButtonText title="Fligro" isActive/>
+        </li>
+        <li>
+          <ButtonText title="Tramólha" />
+        </li>
+        <li>
+          <ButtonText title="Passáro Neves" />
+        </li>
+
+      </Menu>
+
+      <Search>
+      </Search>
+
+      <Content>
+      </Content>
+
+      <NewNote>
+        <FiPlus/>
+        Criar nota
+      </NewNote>
+
     </Container>
   )
 }
--- react/vite-project/src/pages/Home/styles.js
+++ react/vite-project/src/pages/Home/styles.js
@@ -1,5 +1,87 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
   display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: 105px 128px auto 64px;
+  grid-template-areas:
+    "brand header"
+    "menu search"
+    "menu content"
+    "newnote content";
+
+  background: ${ ({ theme }) => theme.COLORS.BACKGROUND_800 };
 `;
+
+export const Brand = styled.div`
+  display: flex;
+  justify-content: center;
+
+  grid-area: brand;
+
+  font-size: 24px;
+
+  width: 250px;
+  padding-block: 39px;
+  border-bottom: 1px solid ${ ({ theme }) => theme.COLORS.BACKGROUND_700 };
+
+  background-color: ${ ({ theme }) => theme.COLORS.BACKGROUND_900 };
+  color: ${ ({ theme }) => theme.COLORS.ORANGE };
+
+`;
+
+export const Menu = styled.ul`
+  grid-area: menu;
+
+  padding: 64px;
+
+  text-align: center;
+  
+  /* background-color: red; */
+  background-color: ${ ({ theme }) => theme.COLORS.BACKGROUND_900 };
+
+  list-style: none;
+
+  > li {
+    margin-bottom: 24px;
+  }
+
+`;
+
+export const Search  = styled.div`
+  grid-area: search;
+  /* background-color: yellow; */
+
+` 
+
+export const Content = styled.div`
+  grid-area: content;
+  /* background-color: green; */
+
+` 
+
+export const NewNote = styled.button`
+  grid-area: newnote;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  /* background-color: blue; */
+  background-color: ${ ({ theme }) => theme.COLORS.ORANGE };
+
+  text-align: center;
+  font-size: 14px;
+
+  border: none;
+
+  svg {
+    margin-right: 8px;
+  }
+
+
+
+`
