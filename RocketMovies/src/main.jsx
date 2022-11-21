db815028ec1daad5bc2f9095adbd2045b1bbfc64
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-21 14:45:56 -0300

    navigation routes created

--- RocketMovies/src/main.jsx
+++ RocketMovies/src/main.jsx
@@ -5,14 +5,14 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global';
 
-import { Details } from './pages/Details'
+import { Routes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Details />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
