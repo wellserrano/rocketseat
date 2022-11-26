54988b22b14341631457b85bd4bcd0abb2dd1ed0
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-26 16:29:54 -0300

    files handling completed

--- stage8/src/routes/users.routes.js
+++ stage8/src/routes/users.routes.js
@@ -1,17 +1,27 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
 
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
-
 const UsersControllers = require("../controllers/UsersControllers")
+
+const userAvatarController = new UserAvatarController();
 const usersControllers = new UsersControllers();
 
-usersRoutes.get("/-", usersControllers.checkStatus)
 usersRoutes.post("/", usersControllers.create)
-usersRoutes.get("/", usersControllers.read)
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update)
 usersRoutes.delete("/", ensureAuthenticated, usersControllers.delete);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 
 module.exports = usersRoutes;
