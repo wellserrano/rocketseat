a501d5b7efa124eff149c71ad34e94a73c293b6f
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-27 12:21:47 -0300

    axios and cors installed and set

--- stage8/src/server.js
+++ stage8/src/server.js
@@ -1,16 +1,21 @@
 require("express-async-errors");
 const express = require("express");
+
 const routes = require("./routes");
+
 const uploadConfig = require("./configs/upload")
 const database = require("./database/sqlite")
 const AppError = require("./utils/AppError")
+
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 database();
 
