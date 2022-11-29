a9dc7024ce547eb9727baab37eb6d611cdfbd5cb
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-29 18:43:40 -0300

    home page rendering movies

--- RocketMovies/src/pages/Home/index.jsx
+++ RocketMovies/src/pages/Home/index.jsx
@@ -1,15 +1,29 @@
 import { Container, Content, TitleAndButton } from "./styles";
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+import { api } from "../../services/api"
 
 import { FiPlus } from 'react-icons/fi'
 
+import { Movie } from "../../components/Movie";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
-import { Movie } from "../../components/Movie";
 
 export function Home() {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchMovies() {
+      const response = await api.get("/movie");
+      setMovies(response.data) 
+
+    }
+
+    fetchMovies();
+
+  }, []);
 
   return (
     <Container>
@@ -22,13 +36,19 @@ export function Home() {
       </TitleAndButton>
 
       <Content>
-          <Movie data={{
-            title: 'Interstellar',
-            rate: 4,
-            description: 'Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.',
-            tags: ['Ficção Científica', 'Drama', 'Família']
+       {
+        movies &&
+        movies.map((movie, key) => (
+          <Movie key={key} data={{
+            title: movie.title,
+            rate: movie.rating,
+            description: movie.description,
+            tags: movie.tags
             }}
           />
+          ))
+       }
+       
 
       </Content>
 
