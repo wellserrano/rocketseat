69c5da2a59d9c0c345089b9dd0d7fc9225913109
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-11-28 21:54:48 -0300

    Migrations altered. UserMovies relational table removed

--- stage8/src/database/knex/migrations/20221114143134_createMovies.js
+++ stage8/src/database/knex/migrations/20221114143134_createMovies.js
@@ -6,6 +6,7 @@ exports.up = knex => knex.schema.createTable("movies",
     table.integer("year").notNullable();
     table.text("director");
     table.integer("rating");
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
   }
